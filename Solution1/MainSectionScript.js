5c03b653cdab06a35a9fd87a3fbaa267de3e879f
minorscarf <minorscarf@gmail.com>
2025-05-24 18:52:47 +0900

    Revert "テンプレート"
    This reverts commit f0eaa58d9dcd2cd2a2ea04c484a7fb6d03c1943e.

--- Solution1/MainSectionScript.js
+++
@@ -1,7 +0,0 @@
-import { fetchImages } from './js/heroSlider.js';
-import { loadFeatureImages } from './js/featureLoader.js';
-import { loadPortfolioImages } from './js/portfolioLoader.js';
-
-fetchImages();
-loadFeatureImages();
-loadPortfolioImages();
